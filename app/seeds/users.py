56d6f23139d58bbb749f84dfcb770306433ea4cc
Kevin <kevindbaik@gmail.com>
2023-10-03 20:44:31 -0700

    fixed seeders for users

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,15 +1,19 @@
 from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
+from faker import Faker
 
+fake = Faker()
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
+    random_date_time = fake.date_time_between(start_date='-1y', end_date='now')
+
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', first_name='Demo', last_name='User', created_at=random_date_time, updated_at=random_date_time)
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', first_name='Marnie', last_name='Dee', created_at=random_date_time, updated_at=random_date_time)
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', first_name='Bobbie', last_name='Bee', created_at=random_date_time, updated_at=random_date_time)
 
     db.session.add(demo)
     db.session.add(marnie)
@@ -28,5 +32,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
