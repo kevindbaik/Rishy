80ca654260096e4c0f3226e31c651def0152befc
Kevin <kevindbaik@gmail.com>
2023-10-10 07:52:32 -0700

    fixed nav

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -2,7 +2,10 @@ import './LandingPage.css'
 
 function LandingPage() {
   return(
-    <h1 id='landing-header'>LANDING PAGE</h1>
+    <div>
+      <h1>Discover new inpirations.</h1>
+      <img className='landing-image' src="https://i.ibb.co/KXYG5X9/tumblr-9bc0973f07d2c84a0d316c90ba7f454e-ab652e35-2048-fotor-20231009223647.png"></img>
+    </div>
   )
 };
 
