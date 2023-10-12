a22a52a627743fa56aae325d178e67ec51bedd5a
Kevin <kevindbaik@gmail.com>
2023-10-12 11:20:11 -0700

    initial styling for landing complete

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -2,10 +2,19 @@ import './LandingPage.css'
 
 function LandingPage() {
   return(
-    <div>
-      <h1 id='landing-header'>**LANDING PAGE COMING VERY VERY SOON**</h1>
-      {/* <h1>Discover new inpirations.</h1>
-      <img className='landing-image' src="https://i.ibb.co/KXYG5X9/tumblr-9bc0973f07d2c84a0d316c90ba7f454e-ab652e35-2048-fotor-20231009223647.png"></img> */}
+    <div className='landing-bgimage-container'>
+      <div id='landing-header-container'>
+        <h3 className='landing-header-1'>Explore endless inspirations.</h3>
+        <h4 className='landing-header-2'>Join the Rishy community, express your creative self, share personal experiences</h4>
+        <h4 className='landing-header-3'>...all with the sound of music.</h4>
+        <button className='landing-join-button'>Get Started</button>
+        <button className='landing-demo-button'>Visit as Demo User</button>
+      </div>
+      <div className='landing-bgimage1'></div>
+      <div className='landing-bgimage2'></div>
+      <div className='landing-bgimage3'></div>
+      <div className='landing-bgimage4'></div>
+      <div className='landing-bgimage5'></div>
     </div>
   )
 };
