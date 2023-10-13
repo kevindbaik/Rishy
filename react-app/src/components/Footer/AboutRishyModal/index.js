f637deb3d20ce3c3c5cd27569162494c6a08a486
Kevin <kevindbaik@gmail.com>
2023-10-13 13:26:22 -0700

    fixed song navigation to change to existing songs

--- react-app/src/components/Footer/AboutRishyModal/index.js
+++ react-app/src/components/Footer/AboutRishyModal/index.js
@@ -8,7 +8,7 @@ function AboutPetsyModal() {
       <h4>About Rishy.</h4>
       <p>Welcome to Rishy, a music sharing social media platform where users can post songs they're currently listening to, along with an image that encapsulates their present feelings. The emotional experience of listening to a song can be significantly enhanced with the visuals of an image. We at Rishy believe that sharing music can be a way to build strong personal relationships and foster special close-knit communities.</p>
       <p>
-      This capstone is dedicated to my best friend Rishi Malani, who loved nothing more than sharing and conversing about the power of music.
+      This capstone is dedicated to my best friend Rishi Malani, who loved nothing more than sharing and conversing about the power of music with others.
       </p>
     </div>
   );
