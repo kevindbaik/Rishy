0ef612eb7773ab5478644a5b8582a347c2fce3d8
Kevin <kevindbaik@gmail.com>
2023-10-18 16:54:41 -0700

    created playlist model

---
+++ app/models/playlist.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Playlist(db.Model):
+  __tablename__ = "playlists"
+
+  if environment == "production":
+      __table_args__ = {'schema': SCHEMA}
+
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+  name = db.Column(db.String(100), nullable=False)
+  description = db.Column(db.String(150))
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+  updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+  user = db.relationship("User", back_populates="playlists")
+
+  def to_dict(self):
+    return {
+        'id' : self.id,
+        'userId' : self.user_id,
+        'name' : self.name,
+        'description' : self.description,
+        'createdAt': self.created_at.strftime('%B %d, %Y'),
+        'updatedAt': self.updated_at.strftime('%B %d, %Y'),
+    }
