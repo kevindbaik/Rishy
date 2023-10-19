a459bfb1f197d1e1ce05198af8cd1ce4e8dc9d3f
Kevin <kevindbaik@gmail.com>
2023-10-19 13:49:40 -0700

    created api for updating a playlist

--- app/api/playlist_routes.py
+++ app/api/playlist_routes.py
@@ -6,7 +6,7 @@ from ..forms import PlaylistForm
 
 playlist_routes = Blueprint("playlists", __name__)
 
-@playlist_routes.route("/<int:playlistId/new", method=["POST"])
+@playlist_routes.route("/new", methods=["POST"])
 @login_required
 def create_playlist():
   """
@@ -18,7 +18,29 @@ def create_playlist():
     new_playlist = Playlist(user_id=current_user.id, name=form.data['name'], private=form.data['private'])
     db.session.add(new_playlist)
     db.session.commit()
-    return jsonify(new_playlist.to_dict())
+    return jsonify(new_playlist.to_dict()), 200
+  return {"errors": form.errors}, 400
+
+@playlist_routes.route("/<int:playlistId>", methods=["PUT"])
+@login_required
+def update_playlist(playlistId):
+  """
+  UPDATE A PLAYLIST
+  """
+  updated_playlist = Playlist.query.get(playlistId)
+  if not updated_playlist:
+    return jsonify({'error' : 'Playlist not found'}), 404
+  if updated_playlist.user_id != current_user.id:
+    return jsonify({'error' : 'Playlist does not belong to user'}), 403
+
+  form = PlaylistForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  if form.validate_on_submit():
+    updated_playlist.name = form.data['name']
+    updated_playlist.private = form.data['private']
+    db.session.commit()
+    return jsonify(updated_playlist.to_dict()), 200
+  return {"errors": form.errors}, 400
 
 @playlist_routes.route("/<int:playlistId>/add", methods=["POST"])
 @login_required
