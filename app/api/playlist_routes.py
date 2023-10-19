d33fa640ee8d07ee3941f3cdb812601adaec10de
Kevin <kevindbaik@gmail.com>
2023-10-19 13:59:00 -0700

    created api for delete playlist

--- app/api/playlist_routes.py
+++ app/api/playlist_routes.py
@@ -29,9 +29,9 @@ def update_playlist(playlistId):
   """
   updated_playlist = Playlist.query.get(playlistId)
   if not updated_playlist:
-    return jsonify({'error' : 'Playlist not found'}), 404
+    return jsonify({'error' : 'playlist not found'}), 404
   if updated_playlist.user_id != current_user.id:
-    return jsonify({'error' : 'Playlist does not belong to user'}), 403
+    return jsonify({'error' : 'playlist does not belong to user'}), 403
 
   form = PlaylistForm()
   form['csrf_token'].data = request.cookies['csrf_token']
@@ -42,6 +42,23 @@ def update_playlist(playlistId):
     return jsonify(updated_playlist.to_dict()), 200
   return {"errors": form.errors}, 400
 
+@playlist_routes.route("/<int:playlistId>", methods=["DELETE"])
+@login_required
+def delete_playlist(playlistId):
+  """
+  DELETE A PLAYLIST
+  """
+  deleted_playlist = Playlist.query.get(playlistId)
+  if not deleted_playlist:
+    return jsonify({'error' : 'playlist not found'}), 404
+  if deleted_playlist.user_id != current_user.id:
+    return jsonify({'error' : 'playlist does not belong to user'}), 403
+
+  db.session.delete(deleted_playlist)
+  db.session.commit()
+
+  return {'message': 'playlist successfully deleted'}, 200
+
 @playlist_routes.route("/<int:playlistId>/add", methods=["POST"])
 @login_required
 def add_post_to_playlist(playlistId):
@@ -50,19 +67,19 @@ def add_post_to_playlist(playlistId):
   """
   post_id = request.json.get('postId')
   if not post_id:
-    return jsonify({'error' : 'No post ID provided'}), 400
+    return jsonify({'error' : 'no post ID provided'}), 400
 
   playlist = Playlist.query.get(playlistId)
   post = Post.query.get(post_id)
   if not playlist or not post:
-    return jsonify({'error' : 'Invalid playlist or post ID'}), 404
+    return jsonify({'error' : 'invalid playlist or post ID'}), 404
 
   if playlist.user_id != current_user.id:
-    return jsonify({'error' : 'Playlist does not belong to user'}), 403
+    return jsonify({'error' : 'playlist does not belong to user'}), 403
 
   if post not in playlist.posts:
     playlist.posts.append(post)
     db.session.commit()
-    return jsonify({'success' : 'Post added to playlist'}), 200
+    return jsonify({'success' : 'post added to playlist'}), 200
   else:
-    return jsonify({'error' : 'Post already in playlist'}), 400
+    return jsonify({'error' : 'post already in playlist'}), 400
